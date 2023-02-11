9cfa800567b0e3331694d845d10d92ea1ab8095c
John Comeau <jc@unternet.net>
2023-02-10 19:13:32 -0700

    refactored into classes, doesn't lint

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
-all: addressing.doctest
+SOURCES = $(wildcard *.py)
+DOCTESTS = $(SOURCES:.py=.doctest)
+LINT = $(SOURCES:.py=.pylint)
+all: $(DOCTESTS) $(LINT)
 %.doctest: %.py
 	python3 -m doctest $<
+%.pylint: %.py
+	pylint3 $<
--- addressing.py
+++ addressing.py
@@ -3,26 +3,60 @@
 Implementation of ideas in the addressing RFC
 '''
 
-def format_0(latitude, longitude, direction, streetword, position):
+class Encoders():
     '''
-    first two args are latitude and longitude
-    third arg is the general direction of the street: 'N' for N-S, 'E' for E-W
-    4th arg is the word for 'street'
-    final arg is the position of the word for 'street' in the address.
+    encoding formats for distributed addressing
+    '''
+    def format_0(latitude, longitude, direction, streetword, position):
+        '''
+        first two args are latitude and longitude
+        third arg is the general direction of the street:
+            'N' for N-S, 'E' for E-W
+        4th arg is the word for 'street'
+        final arg is the position of the word for 'street' in the address.
 
-    >>> format_0(24.067714, -110.316302, 'N', 'Calle', 0)
-    'Calle GPSW110.316302 24.067714N'
+        >>> Encoders.format_0(24.067714, -110.316302, 'N', 'Calle', 0)
+        'Calle GPSW110.316302 24.067714N'
+        '''
+        address = ['GPS']
+        if direction == 'N':
+            address[0] += 'W' if longitude < 0 else 'E'
+            address[0] += str(abs(longitude))
+            address.append(str(abs(latitude)))
+            address[1] += 'S' if latitude < 0 else 'N'
+        else:
+            address[0] += 'S' if latitude < 0 else 'N'
+            address[0] += str(abs(latitude))
+            address.append(str(abs(longitude)))
+            address[1] += 'W' if longitude < 0 else 'E'
+        address.insert(position, streetword)
+        return ' '.join(address)
+
+class Decoders():
     '''
-    address = ['GPS']
-    if direction == 'N':
-        address[0] += 'W' if longitude < 0 else 'E'
-        address[0] += str(abs(longitude))
-        address.append(str(abs(latitude)))
-        address[1] += 'S' if latitude < 0 else 'N'
-    else:
-        address[0] += 'S' if latitude < 0 else 'N'
-        address[0] += str(abs(latitude))
-        address.append(str(abs(longitude)))
-        address[1] += 'W' if longitude < 0 else 'E'
-    address.insert(position, streetword)
-    return ' '.join(address)
+    decoding the various formats back into raw GPS coordinates
+
+    >>> Decoders.format_0('Calle GPSW110.316302 24.067714N', 'Calle')
+    (24.067714, -110.316302, 'N', 'Calle', 0)
+    '''
+    def format_0(address, streetword):
+        '''
+        reconstruct and return args for encode.format0(...)
+        '''
+        parts = address.split()
+        position = parts.index(streetword)
+        parts.pop(position)
+        first = parts[0][3:]
+        if first.startswith(('E', 'W')):
+            direction = 'N'
+            easting, first = first[0], first[1:]
+            northing, second = parts[1][-1], parts[1][:-1]
+            longitude = float(first) * (-1 if easting == 'W' else 1)
+            latitude = float(second) * (-1 if northing == 'S' else 1)
+        else:
+            direction = 'E'
+            northing, first = first[0], first[1:]
+            easting, second = parts[1][-1], parts[1][:-1]
+            longitude = float(second) * (-1 if easting == 'W' else 1)
+            latitude = float(first) * (-1 if northing == 'S' else 1)
+        return (latitude, longitude, direction, streetword, position)
