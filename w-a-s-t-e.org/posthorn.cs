be23bb61e6a2a2d389891892d47de1756cc5bf16
John Comeau <jc@unternet.net>
2024-03-10 17:46:05 -0700

    better

--- w-a-s-t-e.org/posthorn.cs
+++ w-a-s-t-e.org/posthorn.cs
@@ -6,7 +6,7 @@ pagesize /pagewidth exch def /pageheight exch def
 /radius segment 2 div def
 1 inch 4 inch moveto
 segment 0 rlineto  # mouthpiece to start of loop
-currentpoint radius sub radius 0 360 arc  # the loop
+currentpoint radius sub radius 90 450 arc  # the loop
 segment 0 rlineto  # loop to start of bell
 stroke
 showpage
