83574eec3d4e70b97fb212078d8f3cfa5ba9e1a7
John Comeau <jc@unternet.net>
2024-03-10 17:40:30 -0700

    now can see better where it went wrong

--- w-a-s-t-e.org/posthorn.cs
+++ w-a-s-t-e.org/posthorn.cs
@@ -2,13 +2,11 @@
 # muted post horn from Crying of Lot 49
 /inch {72 mul} bind def
 pagesize /pagewidth exch def /pageheight exch def
-/segment 3 def  # see README.md
+/segment 3 inch def  # see README.md
 /radius segment 2 div def
-4 inch 4 inch moveto
+1 inch 4 inch moveto
 segment 0 rlineto  # mouthpiece to start of loop
 currentpoint radius sub radius 0 360 arc  # the loop
 segment 0 rlineto  # loop to start of bell
-pagewidth 2 div pageheight 2 div translate
-1 inch 1 inch scale
 stroke
 showpage
