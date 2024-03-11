abf9a39fcd3ad82ab35c2a10684d483e6554a2eb
John Comeau <jc@unternet.net>
2024-03-10 17:36:02 -0700

    moved towards center of page just to see; weird

--- w-a-s-t-e.org/posthorn.cs
+++ w-a-s-t-e.org/posthorn.cs
@@ -4,7 +4,7 @@
 pagesize /pagewidth exch def /pageheight exch def
 /segment 3 def  # see README.md
 /radius segment 2 div def
-0 0 moveto
+4 inch 4 inch moveto
 segment 0 rlineto  # mouthpiece to start of loop
 currentpoint radius sub radius 0 360 arc  # the loop
 segment 0 rlineto  # loop to start of bell
