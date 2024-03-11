1020b9d7b5cd821589f3c7e1bb97abc8893ba068
John Comeau <jc@unternet.net>
2024-03-11 01:43:47 -0700

    comments with # don't work inside functions

--- w-a-s-t-e.org/posthorn.cs
+++ w-a-s-t-e.org/posthorn.cs
@@ -1,19 +1,21 @@
 #!/usr/local/casperscript/bin/cs
-# muted post horn from Crying of Lot 49
+% muted post horn from Crying of Lot 49
 /inch {72 mul} bind def
 /unit {0.25 inch mul} bind def
 pagesize /pagewidth exch def /pageheight exch def
-/segment 3 unit def  # see README.md
+/segment 3 unit def  % see README.md
 /radius segment 2 div def
-/secant {cos 1.0 exch div} bind def  # hypotenuse over near
+/secant {cos 1.0 exch div} bind def  % hypotenuse over near
 /bell 30 secant segment mul def 
 1 inch 4 inch moveto
-segment 0 rlineto  # mouthpiece to start of loop
-currentpoint radius sub radius 90 450 arc  # the loop
-segment 0 rlineto  # loop to start of bell
-30 rotate  bell 0 rlineto
--120 rotate bell 0 rlineto
--120 rotate bell 0 rlineto
--150 rotate  # reorient left to right
-stroke
+/posthorn {
+  segment 0 rlineto  % mouthpiece to start of loop
+  currentpoint radius sub radius 90 450 arc  % the loop
+  segment 0 rlineto  % loop to start of bell
+  30 rotate  bell 0 rlineto
+  -120 rotate bell 0 rlineto
+  -120 rotate bell 0 rlineto
+  -150 rotate  % reorient left to right
+} bind def
+posthorn stroke
 showpage
