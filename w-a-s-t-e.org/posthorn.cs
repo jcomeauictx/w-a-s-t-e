1013684f71c539bae9dd10776a5fafc3884f5d26
John Comeau <jc@unternet.net>
2024-03-11 20:35:16 -0700

    got rid of artifact from `measure`

--- w-a-s-t-e.org/posthorn.cs
+++ w-a-s-t-e.org/posthorn.cs
@@ -3,7 +3,6 @@
 /inch {72 mul} bind def
 /unit {0.25 inch mul} bind def
 pagesize /pagewidth exch def /pageheight exch def
-1 unit 5 div setlinewidth
 /segment 3 unit def  % see README.md
 /radius segment 2 div def
 /secant {cos 1.0 exch div} bind def  % hypotenuse over near
@@ -18,6 +17,7 @@ pagesize /pagewidth exch def /pageheight exch def
   pathrect erase
 } bind def
 /posthorn {
+  1 unit 5 div setlinewidth
   segment 0 rlineto  % mouthpiece to start of loop
   currentpoint radius sub radius 90 450 arc  % the loop
   segment 0 rlineto  % loop to start of bell
@@ -41,5 +41,5 @@ pagesize /pagewidth exch def /pageheight exch def
   posthorn stroke
   grestore
 } bind def
-measure
+measure newpath
 1 inch 4 inch moveto muted-posthorn showpage
