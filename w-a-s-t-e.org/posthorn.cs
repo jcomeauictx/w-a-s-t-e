96e8335c22ea693392693eb6fdc554233c17f2a2
John Comeau <jc@unternet.net>
2024-03-11 20:06:41 -0700

    working on finishing touches

--- w-a-s-t-e.org/posthorn.cs
+++ w-a-s-t-e.org/posthorn.cs
@@ -9,7 +9,6 @@ pagesize /pagewidth exch def /pageheight exch def
 /secant {cos 1.0 exch div} bind def  % hypotenuse over near
 /bell 30 secant segment mul def
 /erase {gsave 1 setgray rectfill grestore} bind def
-1 inch 4 inch moveto
 /posthorn {
   segment 0 rlineto  % mouthpiece to start of loop
   currentpoint radius sub radius 90 450 arc  % the loop
@@ -19,11 +18,23 @@ pagesize /pagewidth exch def /pageheight exch def
   -120 rotate bell 0 rlineto
   -150 rotate  % reorient left to right
 } bind def
-gsave posthorn stroke grestore
--1 unit 0 rmoveto currentpoint newpath moveto
-gsave posthorn strokepath pathbbox  % gives bounding box llx lly urx ury
-% need to convert to x y width height for rectfill
-2 index sub exch 3 index sub exch
-erase grestore
-posthorn stroke
-showpage
+/measure {  % let's measure the muted posthorn so as to render it best
+  gsave
+  0 0 moveto
+  gsave posthorn strokepath pathbbox
+  pstack clear
+} bind def
+/muted-posthorn {  % the ultimate form
+  gsave
+  1 unit 0 rmoveto
+  gsave posthorn stroke grestore
+  -1 unit 0 rmoveto currentpoint newpath moveto
+  gsave posthorn strokepath pathbbox  % gives bounding box llx lly urx ury
+  % need to convert to x y width height for rectfill
+  2 index sub exch 3 index sub exch
+  erase grestore
+  posthorn stroke
+  grestore
+} bind def
+measure
+1 inch 4 inch moveto muted-posthorn showpage
